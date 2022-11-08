6a8dbbabef3d7e493b70ea9faa34d96f5be10784
DESKTOP-7E6K0DS\31943 <3194329981@qq.com>
2022-11-08 20:57:00 +0800

    提交DB

---
+++ day03/src/preview/JDBC/JDBCTest.java
@@ -0,0 +1,23 @@
+package preview.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JDBCTest {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://127.0.0.1:3306/day02";
+        String username = "root";
+        String password = "123456";
+
+        Connection conn = DriverManager.getConnection(url, username, password);//连接数据库
+
+        Statement stmt = conn.createStatement();//获取执行sql对象
+
+        String sql = "select jname from job";//sql语句
+        boolean result = stmt.execute(sql);//执行语句
+
+        System.out.println(result);
+    }
+}
