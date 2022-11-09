914233e0293d846e4556755d42e890cd5d34b570
DESKTOP-7E6K0DS\31943 <3194329981@qq.com>
2022-11-09 15:21:32 +0800

    提交DB

---
+++ day03/src/preview/JDBC/MysqlCommitDemo02.java
@@ -0,0 +1,30 @@
+package preview.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MysqlCommitDemo02 {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql:///dau03?useSSL=false";
+        String username = "root";
+        String password = "123456";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement stmt = connection.createStatement();
+
+//        CRUD
+
+
+        String sql1 = "update user set money = 400 where name = '李达' ";
+        int re1 = stmt.executeUpdate(sql1);
+        System.out.println(re1);
+
+        int s = 3/0;
+
+        String sql2 = "update user set money = 500 where name = '李达' ";
+        int re2 = stmt.executeUpdate(sql2);
+        System.out.println(re2);
+    }
+}
